375c2c063f31db7f866a06c528ba5504122ba2c4
Maxim <64529642+yam86rus@users.noreply.github.com>
2020-10-19 15:15:36 +0500

    Add files via upload
    up

---
+++ src/Homework/Lesson23/p1/X.java
@@ -0,0 +1,8 @@
+package Homework.Lesson23.p1;
+
+public class X {
+    X(){}
+    public void abc(){
+        System.out.println("X");
+    }
+}
---
+++ src/Homework/Lesson23/p2/Y.java
@@ -0,0 +1,15 @@
+//package Homework.Lesson23.p2;
+//
+//import Homework.Lesson23.p1.*;
+//
+////public class Y extends X {
+////    Y() {} // class X имеет acces modifier default и не виден в пакете p2
+//    public void abc(){
+//        System.out.println("Y");
+//    }
+//
+//    public static void main(String[] args) {
+//        Y y = new Y();
+//        y.abc();
+//    }
+//}
