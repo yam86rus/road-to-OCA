b264baa935dfe347ff0303d58b582070c013a9e1
yam86rus <yam86rus@gmail.com>
2020-11-15 23:41:38 +0500

    up

---
+++ src/Lesson28/Test1.java
@@ -0,0 +1,9 @@
+package Lesson28;
+import java.time.*;
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now());
+    }
+}
